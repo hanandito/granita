cd43430347ca815621cbdcd3776cbd24f4bef60d
teddypicker <hananditos@gmail.com>
2020-11-30 16:41:07 +0700

    app js update

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@
    })
    $('.carousel-products').addClass('owl-carousel owl-theme').owlCarousel({
    loop: true,
+   navText: ["<img src='../images/carousel-nav-07.png'>","<img src='../images/carousel-nav-08.png'>"],
    responsive:{
      0: {
        items: 1,
