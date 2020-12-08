277dc28501e7dceffb22e074017fb2c23d2f8b18
teddypicker <hananditos@gmail.com>
2020-12-08 17:02:51 +0700

    initial commit

--- js/app.js
+++ js/app.js
@@ -89,12 +89,12 @@ $(document).ready(function(){
 
 $( document ).ready(function() {
   $(window).on("scroll resize", function () {
-    var pos = $('.pagi-nations').offset();
+    var pos = $('.number').offset();
     $('.postery').each(function () {
       if (pos.top >= $(this).offset().top && pos.top <= $(this).next().offset().top) {
         var countery = $(this).find('.countery');
-        $('.pagi-nations').html(countery.text());
-        $('.pagi-nations').prop('href',countery.data('href'));
+        $('.number').html(countery.text());
+        $('.number').prop('href',countery.data('href'));
         return; 
       }
     });
