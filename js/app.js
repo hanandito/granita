8935f3bbc50478c9d4141d73b383bbfd5feb42f0
teddypicker <hananditos@gmail.com>
2020-12-11 15:16:43 +0700

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-    /*Animation: Moving Ornament on Scroll*/
-    $(document).ready(function(){
+$(document).ready(function(){
 $('.bubble-origin').click(function() {
   $(".title-content").addClass("active");
    $(".content-detail").addClass("active");
    $(".bubble-origin").addClass("active");
    $(".close-bubble").addClass("active");
 })
-$('.close-bubble').click(function() {
+$('.fa-times').click(function() {
   $(".title-content").removeClass("active");
    $(".content-detail").removeClass("active");
    $(".bubble-origin").removeClass("active");
